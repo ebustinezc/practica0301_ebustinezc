40252935e5c5029cd92e9fd055f4216b6f710818
ebustinezc <ebustinezc@educacion.navarra.es>
2025-01-13 11:58:11 +0100

    Ejercicio2

---
+++ Ejercicio2.py
@@ -0,0 +1,5 @@
+# Escribir una función que lea dos ficheros csv con una lista larga de datos de personas (50 personas y 1000 personas) 
+# y llame a dos funciones que pongan su nombre en formato capitalizado y calculen la letra de DNI correspondiente. 
+# Realiza la comprobación de rendimiento con la librería cProfile y muestra los datos. Describe que indica cada dato 
+# que nos proporciona cProfile.
+
