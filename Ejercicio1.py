20dae7ac84f4f1b46fb0353266e7a43b7d27ca40
ebustinezc <ebustinezc@educacion.navarra.es>
2025-01-13 10:44:06 +0100

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,4 @@
+# Escribir dos funciones, una función que reciba un número entero positivo n y calcule el número de fibonacci 
+# asociado a ese número de manera recursiva y otra función que haga la misma operación pero con bucles.
+# Con ambas funciones, calcular y comparar el tiempo de ejecución para n = (1, 10, 20, 30 y 40) por fuerza bruta.
+
